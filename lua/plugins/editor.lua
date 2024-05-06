ea2d429a3e7ff55eea6072f448b162fc2e57116b
Yuri FA <yurik45gb@gmail.com>
2024-05-06 19:16:41 +0300

    update config

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -8,4 +8,21 @@ return {
     "junegunn/fzf.vim",
     dependencies = { "junegunn/fzf", build = ":call fzf#install()" },
   },
+
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        position = "right",
+      },
+      filesystem = {
+        filtered_items = {
+          -- visible = false, -- when true, they will just be displayed differently than normal items
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_hidden = false, -- only works on Windows for hidden files/directories
+        },
+      },
+    },
+  },
 }
